b7e3ae677d696228bedbb1f63df1369a497951bb
Rafael Puchades Marqués <rpuchadm@cfp.upv.es>
2020-03-17 10:49:47 +0100

    error url

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Alert from 'react-bootstrap/Alert';
 
-const Error = () => {
+const Error = ({url}) => {
   return(
     <Alert variant="danger">
         <Alert.Heading>ERROR</Alert.Heading>
+        { url ? <Fragment>{url}<br/></Fragment> : null }
     </Alert>
   );
 }
