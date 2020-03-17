f488635446d833a26b1055b4bc687b68f20b6454
Rafael Puchades Marqués <rpuchadm@cfp.upv.es>
2020-03-17 11:56:07 +0100

    precios exclusivos

--- src/components/Matric/MatricComponent.js
+++ src/components/Matric/MatricComponent.js
@@ -1,9 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Alert from 'react-bootstrap/Alert';
 import Badge from 'react-bootstrap/Badge';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
 
 import Euros from "../Euros"
@@ -11,26 +13,38 @@ import Euros from "../Euros"
 const Precio = ({colectivo,precio,texto}) => {
     if( colectivo ) {
         return(
-            <li>  COL {colectivo}</li>
-        );
+            <ListGroup.Item> <Badge pill variant='primary' ><Euros valor={precio} /></Badge> COL {colectivo}</ListGroup.Item>
+        )
     }
     return(
-    <li>{texto}</li>
-    );
+        <ListGroup.Item> <Badge pill variant='secondary' ><Euros valor={precio} /></Badge> {texto}</ListGroup.Item>
+    )
 }
 
 const ListadoPrecios = ({listadoPrecios}) => {
     return(
-        <ul>
-            { listadoPrecios.map( (pre,i) => <li key={i}>{pre.colectivo}, {pre.texto}</li> )}
-        </ul>
-    );
+        <ListGroup>
+            { listadoPrecios.map( (pre,i) => <Precio key={i} {...pre} /> )}
+        </ListGroup>
+    )
+}
+
+const AvisoColectivos = ({nc}) => {
+    return(
+        <Alert variant='warning'>
+            {nc} precios exclusivos para colectivos UPV
+        </Alert>
+    )
 }
 
 const Precios = ({listadoPrecios,precioObservaciones}) => {
     if( listadoPrecios && listadoPrecios.length > 0 ) {
+        const nc = listadoPrecios.filter( pre => pre.colectivo ).length;
         return(
+            <>
+            { nc ? <AvisoColectivos nc={nc} /> : null }
             <ListadoPrecios listadoPrecios={listadoPrecios} />
+            </>
         )
     }
     if( precioObservaciones ) {
@@ -48,14 +62,14 @@ const MatricComponent = ({idioma,curso}) => {
     return(
         <Container>
             <Row>
-                <Col>
+                <Col xs={12} md={4} >
                     {curso.acronimo}
                     <br/>
                     <Precios {...curso} />
                 </Col>
             </Row>
             <Row>
-                <Col>
+                <Col xs={12} >
                     <hr/>
                     {JSON.stringify( curso )}
                 </Col>
